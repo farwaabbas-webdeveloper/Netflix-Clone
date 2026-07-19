0bbe44ea9a9cad573ce2d19d950ed52237e0c693
farwaabbas-webdeveloper <farwa.a6011@gmail.com>
2026-07-19 02:25:02 -0700

    add-video-using-JS

---
+++ script.js
@@ -0,0 +1,5 @@
+const video = document.getElementById("myVideo");
+
+video.addEventListener("click", function () {
+  window.open("videos/WhatsApp Video 2026-07-08 at 5.03.21 AM.mp4", "_blank");
+});
